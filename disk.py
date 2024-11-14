850c95965caf7f1d68230d666859155d66e5d4e2
AxelBilla <144888254+AxelBilla@users.noreply.github.com>
2024-11-14 01:02:37 +0100

    Add files via upload

---
+++ disk.py
@@ -0,0 +1,78 @@
+#https://discord.com/oauth2/authorize?client_id=1306356201017184298
+
+import os
+import discord
+from discord import app_commands
+from discord.ext import commands
+from discord.ext.commands import has_permissions, MissingPermissions
+from random import randint
+from private.config import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix='/', intents=intents)
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()    
+
+@bot.tree.command(name='disk', description="Display a random line from the server's saved pickup lines.")
+async def returnHorn(ctx):
+    file = open("serverData/" + str(ctx.guild.id) + "/" + "data.json", "r")
+    wordList=file.readlines()
+    if len(wordList)==0:
+        await ctx.response.send_message("I don't have anything to say.")
+    else:
+        await ctx.response.send_message(wordList[randint(0,int(len(wordList)-1))])
+
+@bot.tree.command(name='add_disk', description="Add a new line to the server's saved pickup lines.")
+@app_commands.checks.has_permissions(administrator=True)
+async def addHorn(ctx, txt: str):
+    path = "serverData/" + str(ctx.guild.id)
+    if not os.path.exists(path):
+        os.makedirs(path)
+        open(path+"/data.json","w")
+    file = open(path + "/" + "data.json", "a")
+    file.write(txt+"\n")
+    await ctx.response.send_message(f"\"{txt}\" was successfully added!")
+    
+@bot.tree.command(name='del_disk', description="Remove a line by its content or id from the server's saved pickup lines (* to remove every line).")
+@app_commands.checks.has_permissions(administrator=True)
+async def addHorn(ctx, txt: str):
+    file = open("serverData/" + str(ctx.guild.id) + "/" + "data.json", "r")
+    wordList=file.readlines()
+    check=True
+    if len(wordList)==0:
+        await ctx.response.send_message("You currently do not have any saved pickup lines.")
+    elif txt=="*":
+        wordList=[]
+        fileWrite = open("serverData/" + str(ctx.guild.id) + "/" + "data.json", "w")
+        fileWrite.writelines(wordList)
+        await ctx.response.send_message(f"Saved pick up lines successfully cleared!")
+    else:
+        for i in range(len(wordList)):
+            if wordList[i]==txt+"\n" or str(i+1)==txt:
+                wordList.pop(i)
+                fileWrite = open("serverData/" + str(ctx.guild.id) + "/" + "data.json", "w")
+                fileWrite.writelines(wordList)
+                await ctx.response.send_message(f"\"{txt}\" was successfully removed!")
+                check=False
+                break
+        if check:
+            await ctx.response.send_message(f"\"{txt}\" couldn't be found!")
+
+@bot.tree.command(name='see_disk', description="Display all saved pickup lines.")
+@app_commands.checks.has_permissions(administrator=True)
+async def checkHorn(ctx):
+    file = open("serverData/" + str(ctx.guild.id) + "/" + "data.json", "r")
+    wordList=file.readlines()
+    outString=""
+    if len(wordList)==0:
+        await ctx.response.send_message("You currently do not have any saved pickup lines.")
+    else:
+        for i in range(len(wordList)):
+            wordList[i]=wordList[i].strip("\n")
+            outString=outString+f"[{i+1}]: \"{wordList[i]}\"\n"
+        await ctx.response.send_message(outString)
+
+bot.run(token)
